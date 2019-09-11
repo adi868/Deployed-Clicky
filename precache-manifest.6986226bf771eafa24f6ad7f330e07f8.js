8541afd814f4106672a4f89e19d0a2e8832b69f6
alciancio <alcianciotto@gmail.com>
2019-09-10 21:21:23 -0500

    Updates

---
+++ precache-manifest.6986226bf771eafa24f6ad7f330e07f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5f391fa9b119185794e852cd697a7b",
+    "url": "/Deployed-Clicky/index.html"
+  },
+  {
+    "revision": "1571e6b0302a205649c5",
+    "url": "/Deployed-Clicky/static/css/main.a80a3176.chunk.css"
+  },
+  {
+    "revision": "f9685dc9e821160aa52e",
+    "url": "/Deployed-Clicky/static/js/2.3053b6a4.chunk.js"
+  },
+  {
+    "revision": "1571e6b0302a205649c5",
+    "url": "/Deployed-Clicky/static/js/main.53b56a31.chunk.js"
+  },
+  {
+    "revision": "4ee301ed2b4c17a90bb0",
+    "url": "/Deployed-Clicky/static/js/runtime~main.738a0942.js"
+  }
+]);
